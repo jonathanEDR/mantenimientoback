b817b5fd1ba4433d7a46a583a830c5c1debd2587
jonathanEDR <jonathan5@gmail.com>
2025-10-18 10:53:54 -0500

    IMPLEMNTACION DE MEJORAS ESTADOS DE MONITOREO 50%

--- src/routes/mantenimiento.ts
+++ src/routes/mantenimiento.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import componentesRoutes from './componentes';
 import ordenesTrabajoRoutes from './ordenesTrabajo';
 import dashboardRoutes from './dashboardMantenimiento';
+import estadosMonitoreoComponenteRoutes from './estadosMonitoreoComponente';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/componentes', componentesRoutes);
 router.use('/ordenes', ordenesTrabajoRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/estados-monitoreo-componente', estadosMonitoreoComponenteRoutes);
 
 export default router;
